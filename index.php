1ac693fff46f530f02faeea2298a87a6d3a8a950
Rifda Nabil Ramadani <rifda.nabil42@smk.belajar.id>
2026-02-10 06:44:38 +0800

    Refactor index.php because i chose the wrong file

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-    $listPerawatan= [
-        ["Perawatan 1" ,"Meremajakan Kulit", "Rp 150.000", "img/perawatan1.jpg"],
-        ["Perawatan 2" ,"Merapikan Kuku Anda", "Rp 100.000", "img/perawatan2.jpg"],
-        ["Perawatan 3" ,"Menyegarkan Kulit Kepala", "Rp 50.000", "img/perawatan3.jpg"],
-        ["Perawatan 4" ,"Model Terbaru Mengikuti Tren", "Rp 120.000", "img/perawatan4.jpg"],
+session_start();
+
+if (isset($_GET['reset'])) {
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
+
+if (!isset($_SESSION['stok'])) {
+    $_SESSION['stok'] = [
+        "Dead Poets Society" => 10,
+        "500 Days Of Summer" => 10,
+        "Totto-Chan : The Girl at the Window" => 10
     ];
-       
+}
+
+$filmData= [
+    ["Dead Poets Society","dead.jpg"],
+    ["500 Days Of Summer","summer.jpg"],
+    ["Totto-Chan : The Girl at the Window","toto.jpg"]
+];
 ?>
 
 <!doctype html>
@@ -13,110 +27,75 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Klinik Estetika</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Kali Kali Satu</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-        .carousel-item img{
-            height: 650px;
+        .card-img-top{
+            height: 500px;
             object-fit: cover;
-        }
+        } 
     </style>
+
 </head>
-  <body>    
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary shadow-sm">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Klinik Estetika</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link" href="index.php">Beranda</a>
-            <a class="nav-link" href="#perawatan">Daftar Perawatan</a>
-        </div>
-        <div class="d-flex ms-auto">
-            <a class="btn btn-primary" href="#">Logout</a>
-        </div>
-        </div>
-    </div>
-    </nav>
-
-    <!-- Carousel -->
-    <section id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="img/perawatan1.jpg" class="d-block w-100" alt="Mengaplikasikan Masker Wajah">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-            </div>
-            </div>
-            <div class="carousel-item">
-            <img src="img/perawatan2.jpg" class="d-block w-100" alt="Manicure">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-            </div>
-            <div class="carousel-item">
-            <img src="img/perawatan3.jpg" class="d-block w-100" alt="Keramas">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-            </div>
-            <div class="carousel-item">
-            <img src="img/perawatan4.jpg" class="d-block w-100" alt="Styling Rambut">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Fourth slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+    <body>
+        <!--NAVBAR-->
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand h1" href="index.php">Kali Kali Satu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link" href="#film">Lihat Film</a>
             </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </section>  
-
-    <!-- List Perawatan -->
-    <section id="perawatan" class="container my-5">
-        <h2 class="text-center mb-4">Daftar Perawatan Klinik Estetika</h2>
-        <div class="row row-cols-1 row-cols-md-4 g-4">
-            <?php foreach($listPerawatan as $index => $tampil) : ?>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="<?= $tampil[3]; ?>" class="card-img-top" alt="<?= $tampil[0]; ?>">
-                    <div class="card-body text-center">
-                        <h5 class="card-title"><?= $tampil[0]; ?></h5>
-                        <p class="card-text"><?= $tampil[1]; ?></p>
-                        <p class="card-text"><strong><?= $tampil[2] ; ?></strong></p>
-                        <a href="pesan.php?index=<?= $index ?>" class="btn btn-primary">Pilih Perawatan</a>
+        </nav>
+        <!--HERO-->
+        <section class="container mt-4 text-center">
+            <div class="p-5">
+                <h1 class="fw-bold">Selamat Datang di Kali Kali Satu</h1>
+                <p class="col-md-8 mx-auto">
+                    Tempat terbaik untuk menonton film favorit Anda bersama keluarga dan teman-teman.
+                </p>
+                <a href="#film" class="btn btn-primary btn-lg">Lihat Film</a>
+            </div>
+        </section>
+        <!--CARD-->
+        <section class="container mt-4 text-center" id="film">
+            <div class="row">
+                <?php foreach($filmData as $film): ?>
+                <div class="col-md-4 mb-3">
+                    <div class="card h-100">
+                        <img src="img/<?php echo $film[1]; ?>" class="card-img-top" alt="<?php echo $film[0]; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $film[0]; ?></h5>
+                            <p class="text-muted">Stok tiket: <?= $_SESSION['stok'][$film[0]] ?></p>
+                            <?php if ($_SESSION['stok'][$film[0]] > 0): ?>
+                            <a href="pesan.php?film=<?= urlencode($film[0]) ?>&gambar=<?= $film[1] ?>" 
+                            class="btn btn-primary">
+                            Pesan Tiket
+                            </a>
+                            <?php else: ?>
+                                <button class="btn btn-secondary" disabled>
+                                Stok Habis
+                                </button>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
-        </div>
-    </section>
-    
-    <!--Footer-->
-    <footer class="bg-secondary text-white text-center py-3">
-        &copy; 2024 Klinik Estetika. All rights reserved.
-    </footer>
+        </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
+        <a href="?reset=true" class="btn btn-danger mb-3">
+            Reset Stok
+        </a>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>
+
